86cedb4d21b4264dcfab2ec33b7b8564a2e169fd
unknown <20183l301027@utcv.edu.mx>
2021-07-31 23:32:54 -0500

    resolucion de errores

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CatalogoComponent } from './catalogo/catalogo.component';
+import { InicioComponent } from './inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: InicioComponent},
+  {path: 'catalogo', component: CatalogoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { InfoComponent } from './info/info.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { CatalogoComponent } from './catalogo/catalogo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InfoComponent
-  ],
+    InfoComponent,
+    NavbarComponent,
+    InicioComponent,
+    CatalogoComponent
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule
